215ff937083360770cf6c98a352f16ca70a4dde7
Simina Baumgarten <simina.baumgarten@gmail.com>
2023-10-06 08:02:51 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setSteps] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function handleMinus() {
+    setCount((d) => d - step);
+  }
+
+  function handlePlus() {
+    setCount((d) => d + step);
+  }
+
+  function handleStepsMinus() {
+    setSteps((s) => s - 1);
+  }
+  function handleStepsPlus() {
+    setSteps((s) => s + 1);
+  }
+  const now = new Date();
+  now.setDate(now.getDate() + count);
+
+  return (
+    <>
+      <div>
+        <button onClick={handleStepsMinus}>-</button>
+        <span> Step: {step} </span>
+        <button onClick={handleStepsPlus}>+</button>
+      </div>
+      <div className="padding">
+        <button onClick={handleMinus}>-</button>
+        <span> Count: {count} </span>
+        <button onClick={handlePlus}>+</button>
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{now.toDateString()}</span>
+        </p>
+      </div>
+    </>
+  );
+}
